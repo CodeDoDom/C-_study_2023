7bbe2f0779bfc064f50279eceb5d3db2b3c894cc
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-02-13 12:23:47 +0900

    03장 예제 RacingCarClassBase.cpp

--- 03-2_RacingCarClassBase.cpp
+++ 03-2_RacingCarClassBase.cpp
@@ -18,15 +18,15 @@ private:
 	int fuelGauge;
 	int curSpeed;
 public:
-	void InitMembers(char* ID, int fuel);
+	void InitMembers(const char* ID, int fuel);
 	void ShowCarState();
 	void Accel();
 	void Break();
 };
 
-void Car::InitMembers(char* ID, int fuel)
+void Car::InitMembers(const char* ID, int fuel)
 {
-	strcpy(gamerID, ID);
+	strcpy_s(gamerID, ID);
 	fuelGauge = fuel;
 	curSpeed = 0;
 }
@@ -34,11 +34,45 @@ void Car::InitMembers(char* ID, int fuel)
 void Car::ShowCarState()
 {
 	cout << "소유자ID: " << gamerID << endl;
-	cout << "연료량: " << fuelGauge << endl;
+	cout << "연료량: " << fuelGauge << "%" << endl;
 	cout << "현재속도: " << curSpeed << "km/s" << endl << endl;
+}
+
+void Car::Accel()
+{
+	if (fuelGauge <= 0)
+		return;
+	else
+		fuelGauge -= CAR_CONST::FUEL_STEP;
+
+	if ((curSpeed + CAR_CONST::ACC_STEP) >= CAR_CONST::MAX_SPD)
+	{
+		curSpeed = CAR_CONST::MAX_SPD;
+		return;
+	}
+	curSpeed += CAR_CONST::ACC_STEP;
 }
 
 void Car::Break()
 {
+	if (curSpeed < CAR_CONST::BRK_STEP)
+	{
+		curSpeed = 0;
+		return;
+	}
+	curSpeed -= CAR_CONST::BRK_STEP;
+}
 
+int main(void)
+{
+	Car run99;
+	run99.InitMembers("run99", 100);
+	run99.Accel();
+	run99.Accel();
+	run99.Accel();
+	run99.ShowCarState();
+	run99.Break();
+	run99.ShowCarState();
+
+	return 0;
 }
