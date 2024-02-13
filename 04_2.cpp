0a67e71a837a9f4673d30a934f5c5f00ea27b336
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-02-13 15:03:36 +0900

    문제 04_2 해결

---
+++ 04_2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+	int xpos;
+	int ypos;
+public:
+	void Init(int x, int y)
+	{
+		xpos = x;
+		ypos = y;
+	}
+	void ShowPointInfo() const
+	{
+		cout << "[" << xpos << ", " << ypos << "]" << endl;
+	}
+};
+
+class Circle
+{
+private:
+	int radius;
+	Point center;
+public:
+	void InitCircle(int x, int y, int r)
+	{
+		center.Init(x, y);
+		radius = r;
+	}
+	void ShowCircle() const
+	{
+		cout << "¹ÝÁö¸§: " << radius << endl;
+		center.ShowPointInfo();
+	}
+};
+
+class Ring
+{
+private:
+	Circle inner;
+	Circle outter;
+public:
+	void Init(int x1, int y1, int r1, int x2, int y2, int r2)
+	{
+		inner.InitCircle(x1, y1, r1);
+		outter.InitCircle(x2, y2, r2);
+	}
+	void ShowRingInfo() const
+	{
+		cout << "[ Inner Circle Info ]" << endl;
+		inner.ShowCircle();
+		cout << "[ Outter Circle Info ]" << endl;
+		outter.ShowCircle();
+	}
+};
+
+int main(void)
+{
+	Ring ring;
+	ring.Init(1, 1, 4, 2, 2, 9);
+	ring.ShowRingInfo();
+	return 0;
+}
