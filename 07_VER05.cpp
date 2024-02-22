ac4fb546c153a12bf8319ef0a4e9b08e5e3d7139
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-02-22 23:54:00 +0900

    단계별 프로젝트 07 수정
    멤버함수의 정의 클래스 밖으로 뺌.
    main 수정.
    
    AccountHandler의 생성자 정의에서 accArr 변수가 초기화되지 않았다는 경고 발생.

--- 07_VER05.cpp
+++ 07_VER05.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 #define NAME_LEN 10
 
-void ShowMenu(void);
-
 enum { MAKE = 1, DEPOSIT, WITHDRAW, SHOW, EXIT };
 
 class Account
@@ -16,45 +14,54 @@ private:
 	int accID;
 	int balance;
 public:
-	Account(char* _name, int _accID, int _balance)
-		:accID(_accID), balance(_balance)
-	{
-		name = new char[strlen(_name) + 1];
-		strcpy(name, _name);
-	}
-	Account(const Account& copy)
-		:accID(copy.accID), balance(copy.balance)
-	{
-		name = new char[strlen(copy.name) + 1];
-		strcpy(name, copy.name);
-	}
-	int GetAccID() const
-	{
-		return accID;
-	}
-	void DepositMoney(int money)
-	{
-		balance += money;
-	}
-	int WithdrawMoney(int money)
-	{
-		if (balance < money)
-			return 0;
+	Account(char* _name, int _accID, int _balance);
+	Account(const Account& copy);
 
-		balance -= money;
-		return money;
-	}
-	void ShowAccInfo() const
-	{
-		cout << "계좌주: " << name << endl;
-		cout << "계좌번호: " << accID << endl;
-		cout << "잔액: " << balance << endl;
-	}
-	~Account()
-	{
-		delete[]name;
-	}
+	int GetAccID() const;
+	void DepositMoney(int money);
+	int WithdrawMoney(int money);
+	void ShowAccInfo() const;
+	~Account();
 };
+
+Account::Account(char* _name, int _accID, int _balance)
+	:accID(_accID), balance(_balance)
+{
+	name = new char[strlen(_name) + 1];
+	strcpy(name, _name);
+}
+Account::Account(const Account& copy)
+	:accID(copy.accID), balance(copy.balance)
+{
+	name = new char[strlen(copy.name) + 1];
+	strcpy(name, copy.name);
+}
+int Account::GetAccID() const
+{
+	return accID;
+}
+void Account::DepositMoney(int money)
+{
+	balance += money;
+}
+int Account::WithdrawMoney(int money)
+{
+	if (balance < money)
+		return 0;
+
+	balance -= money;
+	return money;
+}
+void Account::ShowAccInfo() const
+{
+	cout << "계좌주: " << name << endl;
+	cout << "계좌번호: " << accID << endl;
+	cout << "잔액: " << balance << endl;
+}
+Account::~Account()
+{
+	delete[]name;
+}
 
 class AccountHandler
 {
@@ -62,61 +69,22 @@ private:
 	Account* accArr[100];
 	int accCnt = 0;
 public:
-	void MakeAccount(void);
-	void Deposit(void);
-	void Withdraw(void);
-	void ShowInfo(void);
+	AccountHandler();
+	void ShowMenu() const;
+	void MakeAccount();
+	void Deposit();
+	void Withdraw();
+	void ShowInfo() const;
+	~AccountHandler();
 };
 
-int main(void)
-{
-	int cmd;
-
-	while (1)
-	{
-		ShowMenu();
-		cout << "몇 번 기능을 이용하시겠습니까? ";
-		cin >> cmd;
-		cout << endl;
-
-		switch (cmd)
-		{
-		case MAKE:	// 계좌개설
-			MakeAccount();
-			break;
-
-		case DEPOSIT:	// 입금
-			Deposit();
-			break;
-
-		case WITHDRAW:	// 출금
-			Withdraw();
-			break;
-
-		case SHOW:	// 전체고객 잔액조회
-			ShowInfo();
-			break;
-
-		case EXIT:	// 프로그램 종료
-			cout << "프로그램을 종료합니다." << endl;
-			return 0;
-
-		default: // cmd 입력 예외 처리
-			cout << "잘못된 입력입니다. 재입력해주세요." << endl;
-			break;
-		}
-	}
-
-	return 0;
-}
-
-void ShowMenu(void)
+void AccountHandler::ShowMenu() const
 {
 	cout << "[ 은행계좌 관리 프로그램 ]" << endl;
 	cout << "1. 계좌개설" << endl << "2. 입금" << endl << "3. 출금" << endl << "4. 전체고객 잔액조회" << endl << "5. 프로그램 종료" << endl;
 }
 
-void AccountHandler::MakeAccount(void)
+void AccountHandler::MakeAccount()
 {
 	char name[NAME_LEN];
 	int accID;
@@ -136,7 +104,7 @@ void AccountHandler::MakeAccount(void)
 	cout << endl;
 }
 
-void AccountHandler::Deposit(void)
+void AccountHandler::Deposit()
 {
 	int acc;
 	int money;
@@ -168,7 +136,7 @@ void AccountHandler::Deposit(void)
 	cout << endl;
 }
 
-void AccountHandler::Withdraw(void)
+void AccountHandler::Withdraw()
 {
 	int acc;
 	int money;
@@ -205,7 +173,10 @@ void AccountHandler::Withdraw(void)
 	cout << endl;
 }
 
-void AccountHandler::ShowInfo(void)
+AccountHandler::AccountHandler() :accCnt(0)
+{ }
+
+void AccountHandler::ShowInfo() const
 {
 	cout << "[전체고객 잔액조회]" << endl;
 
@@ -214,4 +185,53 @@ void AccountHandler::ShowInfo(void)
 		accArr[i]->ShowAccInfo();
 		cout << endl;
 	}
+}
+
+AccountHandler::~AccountHandler()
+{
+	for (int i = 0; i < accCnt; i++)
+		delete accArr[i];
+}
+
+int main(void)
+{
+	AccountHandler manager;
+	int cmd;
+
+	while (1)
+	{
+		manager.ShowMenu();
+		cout << "몇 번 기능을 이용하시겠습니까? ";
+		cin >> cmd;
+		cout << endl;
+
+		switch (cmd)
+		{
+		case MAKE:	// 계좌개설
+			manager.MakeAccount();
+			break;
+
+		case DEPOSIT:	// 입금
+			manager.Deposit();
+			break;
+
+		case WITHDRAW:	// 출금
+			manager.Withdraw();
+			break;
+
+		case SHOW:	// 전체고객 잔액조회
+			manager.ShowInfo();
+			break;
+
+		case EXIT:	// 프로그램 종료
+			cout << "프로그램을 종료합니다." << endl;
+			return 0;
+
+		default: // cmd 입력 예외 처리
+			cout << "잘못된 입력입니다. 재입력해주세요." << endl;
+			break;
+		}
+	}
+
+	return 0;
 }
