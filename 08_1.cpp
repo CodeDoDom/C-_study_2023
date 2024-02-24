102cbb0f018b73a76b8ad4b3e8f3661c9ba25498
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-02-25 01:49:26 +0900

    문제 08_1 수정_Employee 클래스를 추상 클래스로 수정
    Employee 클래스를 추상 클래스로 정의함.

--- 08_1.cpp
+++ 08_1.cpp
@@ -19,12 +19,8 @@ public:
 	{
 		cout << "name: " << name << endl;
 	}
-	virtual int GetPay() const
-	{
-		return 0;
-	}
-	virtual void ShowSalaryInfo() const
-	{}
+	virtual int GetPay() const = 0;
+	virtual void ShowSalaryInfo() const = 0;
 };
 
 class PermanentWorker :public Employee
